551aca52497fb02f612f458b6f21a99b45d94ad6
deriklim8 <deriklim8@gmail.com>
2022-07-29 15:08:10 +0800

    更改部分指令至嵌入式

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,10 +1,12 @@
-
+const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "ping",
     description: "获取我家的网速",
     run: async (client, message, args, prefix) => {
         message.reply({
-            content: `:ping_pong: **PONG! 我家的网络延迟是: \`${client.ws.ping}ms\`**`
+            embeds: [new MessageEmbed()
+            .setTitle(`:ping_pong: **PONG! 我家的网络延迟是: \`${client.ws.ping}ms\`**`)
+        .setFooter({ text: client.user.tag + '｜._.', iconURL: client.user.avatarURL() })]
         }).catch(() => null);
     },
 };
--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,4 +1,4 @@
-
+const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "uptime",
     description: "获取我宅在discord的总时长",
@@ -6,7 +6,9 @@ module.exports = {
         let date = new Date()
         let timestamp = date.getTime() - Math.floor(client.uptime);
         message.reply({
-            content: `<:duration1:1002230218276225174> **我在<t:${Math.floor(timestamp / 1000)}:F>开始宅在discord并从<t:${Math.floor(timestamp / 1000)}:R>开始了discord的宅家生活。**`
+            embeds: [new MessageEmbed()
+            .setTitle(`<:duration1:1002230218276225174> **我在<t:${Math.floor(timestamp / 1000)}:F>开始宅在discord并从<t:${Math.floor(timestamp / 1000)}:R>开始了discord的宅家生活。**`)]
+            .setFooter({ text: client.user.tag + '｜（＾∀＾●）ﾉｼ', iconURL: client.user.avatarURL() })
         }).catch(() => null);
     },
 };
